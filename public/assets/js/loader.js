7229e53bbd822d491e64959c3aa0831a6a2b54a3
Alexander Novo <alexandernovo84@gmail.com>
2025-08-17 19:54:34 +0800

    loader

--- public/assets/js/loader.js
+++ public/assets/js/loader.js
@@ -1,51 +1,51 @@
 let loader = `
     <div class="totalLoader">
-    <div class="loader">
-        <svg viewBox="0 0 80 80">
-            <rect x="8" y="8" width="64" height="64"></rect>
-            <text
-            x="50%"
-            y="60%"
-            text-anchor="middle"
-            fill="black"
-            font-size="24"
-            font-weight="bold"
-            >
-            L
-            </text>
-        </svg>
+        <div class="loader">
+            <svg viewBox="0 0 80 80">
+                <rect x="8" y="8" width="64" height="64"></rect>
+                <text
+                x="50%"
+                y="60%"
+                text-anchor="middle"
+                fill="black"
+                font-size="24"
+                font-weight="bold"
+                >
+                L
+                </text>
+            </svg>
         </div>
 
         <div class="loader">
-        <svg viewBox="0 0 80 80">
-            <rect x="8" y="8" width="64" height="64"></rect>
-            <text
-            x="50%"
-            y="60%"
-            text-anchor="middle"
-            fill="black"
-            font-size="24"
-            font-weight="bold"
-            >
-            O
-            </text>
-        </svg>
+            <svg viewBox="0 0 80 80">
+                <rect x="8" y="8" width="64" height="64"></rect>
+                <text
+                x="50%"
+                y="60%"
+                text-anchor="middle"
+                fill="black"
+                font-size="24"
+                font-weight="bold"
+                >
+                O
+                </text>
+            </svg>
         </div>
 
         <div class="loader">
-        <svg viewBox="0 0 80 80">
-            <rect x="8" y="8" width="64" height="64"></rect>
-            <text
-            x="50%"
-            y="60%"
-            text-anchor="middle"
-            fill="black"
-            font-size="24"
-            font-weight="bold"
-            >
-            A
-            </text>
-        </svg>
+            <svg viewBox="0 0 80 80">
+                <rect x="8" y="8" width="64" height="64"></rect>
+                <text
+                x="50%"
+                y="60%"
+                text-anchor="middle"
+                fill="black"
+                font-size="24"
+                font-weight="bold"
+                >
+                A
+                </text>
+            </svg>
         </div>
 
         <div class="loader">
@@ -63,7 +63,7 @@ let loader = `
                 </text>
             </svg>
         </div>
-            <div class="loader">
+        <div class="loader">
             <svg viewBox="0 0 80 80">
                 <rect x="8" y="8" width="64" height="64"></rect>
                 <text
@@ -78,7 +78,7 @@ let loader = `
                 </text>
             </svg>
         </div>
-            <div class="loader">
+        <div class="loader">
             <svg viewBox="0 0 80 80">
                 <rect x="8" y="8" width="64" height="64"></rect>
                 <text
@@ -93,7 +93,7 @@ let loader = `
                 </text>
             </svg>
         </div>
-            <div class="loader">
+        <div class="loader">
             <svg viewBox="0 0 80 80">
                 <rect x="8" y="8" width="64" height="64"></rect>
                 <text
@@ -111,29 +111,14 @@ let loader = `
     </div>
 `;
 
-let navEntry;
-const entries = performance.getEntriesByType("navigation");
+let navEntry = performance.getEntriesByType("navigation")[0];
 
-if (entries.length > 0) {
-    navEntry = entries[0];
-} else if (performance.navigation) {
-    switch (performance.navigation.type) {
-        case performance.navigation.TYPE_RELOAD:
-            navEntry = { type: "reload" };
-            break;
-        case performance.navigation.TYPE_BACK_FORWARD:
-            navEntry = { type: "back_forward" };
-            break;
-        default:
-            navEntry = { type: "navigate" };
+if (navEntry) {
+    if (navEntry.type === "reload") {
+        document.body.insertAdjacentHTML("beforeend", loader);
+        setTimeout(() => {
+            const loaderEl = document.querySelector(".totalLoader");
+            if (loaderEl) loaderEl.remove();
+        }, 1000);
     }
-}
-
-if (navEntry.type === "reload") {
-    document.body.insertAdjacentHTML("beforeend", loader);
-
-    setTimeout(() => {
-        const loaderEl = document.querySelector(".totalLoader");
-        if (loaderEl) loaderEl.remove();
-    }, 1000);
 }
