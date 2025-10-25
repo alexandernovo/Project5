62d7d2f71993a3e04fe1bf9e3eef13cef345dc3e
Alexander Novo <alexandernovo84@gmail.com>
2025-10-25 19:53:26 +0800

    update

--- resources/views/home/views/home.blade.php
+++ resources/views/home/views/home.blade.php
@@ -5,10 +5,10 @@
     <div class="d-flex flex-wrap justify-content-between align-items-center gap-4 banner px-5"
         style="height: calc(100vh - 122px);">
         <div class="text-start text-white">
-            <p class="mb-4" style="font-size: 27px">Welcome to</p>
-            <p class="mb-1" style="font-size: 50px; font-weight: bold">BARBAZA MENRO-BAARS</p>
-            <p style="font-size: 24px; font-style: italic">( Municipal Environment and Natural Resources)</p>
-            <p class="mb-1" style="font-size: 22px; margin-top: 70px">
+            <p class="mb-4" style="font-size: 20px">Welcome to</p>
+            <p class="mb-1" style="font-size: 43px; font-weight: bold">BARBAZA MENRO-BAARS</p>
+            <p style="font-size: 20px; font-style: italic">( Municipal Environment and Natural Resources)</p>
+            <p class="mb-1" style="font-size: 18px; margin-top: 70px">
                 "Ensuring Business Legality, Supporting Environment Goals"
             </p>
         </div>
